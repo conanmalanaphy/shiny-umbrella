9abc80f6d5c2ed3440751972f40f7564a5317063
Conan <conanmalanaphy@gmail.com>
2020-03-27 22:07:56 +0000

    lower case the letters

--- src/Core/WhatsThePlan.tsx
+++ src/Core/WhatsThePlan.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { css, jsx } from '@Emotion/core'
+import { css, jsx } from '@emotion/core'
 /////////////////////////////////////
 ///          CSS Styles           ///
 /////////////////////////////////////
--- src/Core/WhatsTheRating.tsx
+++ src/Core/WhatsTheRating.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import { css, jsx } from '@Emotion/core'
+import { css, jsx } from '@emotion/core'
 import SearchBox from './SearchBox'
 // fun
 /// https://codepen.io/yoannhel/pen/sJpDj
